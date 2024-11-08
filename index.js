db9f78898827fb89f89d6a82ab9faef0019d0ae9
jesusesteban2 <jesus.esteban@outlook.com>
2024-11-08 19:52:15 +0100

    Add exercices

---
+++ index.js
@@ -0,0 +1,71 @@
+// 1- Imprime por consola el string 'Hello World'.
+function exer1() {
+  console.log('Hello World');
+}
+
+//2- Declara una variable de las dos formas posibles, e imprime por consola los dos valores.
+function exer2() {
+  let variable1 = 'var1';
+  const variable2 = 'var2';
+  console.log(`variable1: ${variable1} variable2: ${var2}`);
+}
+
+//3- Cambia ahora el valor de una de las dos variables e imprime por consola.
+function exer3() {
+  variable1 = 'variable cambiada';
+  console.log('variable1: ', variable1);
+}
+
+//4- Crea dos variable numéricas e imprime el resultado de sumarlas por consola.
+function exer4() {
+  const sumando1 = 2,
+    sumando2 = 3;
+  console.log('Suma sumando1 + sumando2=', sumando1 + sumando2);
+}
+
+//5- Declara dos variables de tipo string. Imprime por consola el resultado de concatenarlas.
+function exer5() {
+  const greeting1 = 'Hello';
+  const greeting2 = ' World';
+  console.log(greeting1 + greeting2);
+}
+
+//6- Crea una función que imprima por consola el string 'Hello World'.
+function exer6() {
+  console.log('Hello World');
+}
+
+//7- Crea una función que, al ser llamada, imprima por consola el resultado de la multiplicación de dos números introducidos como parámetros.
+function exer7() {
+  const number1 = prompt('Introduce el primer número');
+  const number2 = prompt('Introduce el segundo número');
+  console.log('El resultado del producto es: ', number1 * number2);
+}
+
+//8- Crea una función que imprima por consola el resultado de elevar al cubo un número dado como parámetro.
+function exer8() {
+  const number1 = prompt('Introduce el un número');
+  console.log(`El cuadrado de ${number1} es: `, number1 ** 3);
+}
+
+//9- Crea una función que saque por consola el área de un rectángulo de dimensiones dadas como parámetro. (base, altura)
+function exer9() {
+  const base = prompt('Introduce la base del rectangulo');
+  const altura = prompt('Introduce la altura del rectangulo');
+  console.log('El area del rectangulo es: ', base * altura);
+}
+
+//10- Crea una función que imprima por consola un número al azar entre 0 y 10.
+function exer10() {
+  console.log('Hello World');
+}
+
+//11- Adivina el número: Crea una función que primero guarde en una variable un número aleatorio del 1 al 10. Después, pregunte al usuario a través de un prompt un número del 1 al 10. Una vez recibida la respuesta, compare estos dos números. Si los números coinciden, imprime por consola un string indicando que el usuario ha acertado, sino, imprime por consola que el usuario ha fallado seguido del número correcto.
+
+//12- Crea una función que reciba un número como parámetro e imprima por consola si el número dado es par o impar.
+
+//13- Crea una función que reciba un parámetro de tipo string e imprima por consola el string revertido. (ejemplo: 'casa' => 'asac).
+
+//14- Crea una función que imprima por consola la tabla de multiplicar de un número introducido como parámetro.
+
+//15- Crea una función que reciba un número por parámetros e imprima por consola si el número recibido es un número primo.
